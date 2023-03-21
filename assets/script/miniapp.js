8e34796b5f31e391b155f944be253db0a2f33c36
kamranss <kamranss@code.edu.az>
2023-03-21 15:14:04 +0400

    Refactor: task updated

--- assets/script/miniapp.js
+++ assets/script/miniapp.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
         method: "get",
         success: function(data){
             console.log(data);
+          data.forEach(element => {
+            console.log(element.data.timing.Asr);
+          });
+
+            // let box = d.createElement("div")
+            // box.append(`<p>${data.}</p>
+            // <p></p>
+            // <p></p>`)
+
         },
         error: function(data){
             console.log(data);
