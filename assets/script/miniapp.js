6da3e8148f71ef076bcf4519b5a1107d24df8353
kamranss <kamranss@code.edu.az>
2023-03-21 18:22:29 +0400

    Refactor: Task updated

--- assets/script/miniapp.js
+++ assets/script/miniapp.js
@@ -10,10 +10,28 @@ $(document).ready(function(){
     let month = dateInputArr[1];
     let day  = dateInputArr[2];
 
+    let city = $("#inlineFormSelectPref").val();
+    console.log(city);
     // latitude
     // 40.409264
     // Longitude
     // 49.867092
+
+        let keyTimeZone = "+eTWYvpbfBgFGn4RbZbEpQ==6zUS8yIwq093kBch"
+        $.ajax({
+          url:`https://api.api-ninjas.com/v1/city?name=${city}`,
+          headers: { 'X-Api-Key': keyTimeZone},
+          success: function(data){
+            console.log(data);
+            console.log(data[0].country);
+            console.log(data[0].longitude);
+          },
+          error: function(data){
+            console.log(data);
+          }
+        })
+
+   
       
       $.ajax({
           url: `http://api.aladhan.com/v1/calendar/${year}/${month}?latitude=51.508515&longitude=-0.1254872&method=2`,
@@ -22,6 +40,19 @@ $(document).ready(function(){
           
               data.data.forEach(element => {
               console.log(element);
+                
+              let content = `
+              <tbody>
+                  <tr>
+                    <th scope="row">${element.date.gregorian.day}</th>
+                    <td>${element.meta.timezone}</td>
+                    <td>${element.meta.midnightMode}</td>
+                    <td>${element.meta.latitude}</td>
+                    <td>${element.meta.longitude}</td>
+                  </tr>
+                </tbody>`
+
+                $(".table").append(content);
             });
   
               // let box = d.createElement("div")
